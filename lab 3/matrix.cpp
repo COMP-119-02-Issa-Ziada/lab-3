6aa0a2eb8f1a8ea27e9c9fdc7d0d8b88a8acc257
Issa Ziada <issa000ziadeh@gmail.com>
2026-05-20 13:43:09 +0300

    changes to main and header files

---
+++ lab 3/matrix.cpp
@@ -0,0 +1,8 @@
+//
+//  matrix.cpp
+//  lab 3
+//
+//  Created by Issa's Mac on 20/05/2026.
+//
+
+#include "matrix.hpp"
---
+++ lab 3/matrix.h
@@ -0,0 +1,12 @@
+//
+//  matrix.h
+//  lab 3
+//
+//  Created by Issa's Mac on 20/05/2026.
+//
+
+#ifndef matrix_h
+#define matrix_h
+
+
+#endif /* matrix_h */
