eb70402b181d56cef8d4d63ace85d7bfdf3465cd
Issa Ziada <issa000ziadeh@gmail.com>
2026-05-20 13:26:36 +0300

    Initial Commit

---
+++ lab 3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab 3
+//
+//  Created by Issa's Mac on 20/05/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
